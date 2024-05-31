50ffa269ff192f55eba7beb635f8f6563a2584cd
DeveshNirupam <deveshnirupam1908@gmail.com>
2024-05-31 20:11:20 +0530

    login signup folder created

---
+++ frontend/src/app/about-us/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>
+        <h1 className='text-4xl'>About us</h1>
+    </div>
+  )
+}
+
+export default AboutUs
---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 style={ { textAlign: 'center', fontSize: 56, color: 'blue' }}>Home Page</h1>
+      <input type="text" className='border-2 border-black'/>
+    </div>
+  )
+}
+
+export default Home
